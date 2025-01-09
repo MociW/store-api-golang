433368381451fda6c7238e5dcce02b39fbba26cf
MociW <mochixmoca@gmail.com>
2025-01-09 21:45:35 +0700

    update config

--- db/migrations/20241201223506_create_table.down.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS "product_skus";
-
-DROP TABLE IF EXISTS "products";
-
-DROP TABLE IF EXISTS "product_skus";
-
-DROP TABLE IF EXISTS "addresses";
-
-DROP TABLE IF EXISTS "users";
